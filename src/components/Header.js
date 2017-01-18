f02cfa85cd552871939e5ae5e6579c40afba28be
Ammar Karim <Ammar Karim>
2017-01-18 17:20:43 -0500

    finished header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,16 +4,31 @@
 import React  from 'react';
 import { Header, Segment } from 'semantic-ui-react'
 import './Header.css';
+import Mailto from 'react-mailto';
 
 class HeaderContainer extends React.Component {
     render() {
         return (
-            <div className="ui clearing segment">
-                <h3 className="ui left floated header" id="left">
+            <div className="ui clearing segment" id="ceiling">
+                <h3 className="ui left floated header" id="leftFloater">
                     Ammar Karim
                 </h3>
-                <h3 className="ui right floated header" id="right">
-                    Right
+                <h3 className="ui right floated header rightFloater" >
+
+                    <a className="ui link" href="https://www.linkedin.com/in/ammarkarim">
+                        <i className="linkedin square icon"></i>
+                        LinkedIn
+                    </a>
+                </h3>
+                <h3 className="ui right floated header rightFloater">
+                    {/*<a className="ui link" href="">*/}
+                        {/*<i className="mail icon"></i>*/}
+                        {/*Contact Me*/}
+                    {/*</a>*/}
+                    <Mailto email="me@ammarkarim.com" obfuscate={true}>
+                        <i className="mail icon"></i>
+                        Contact Me
+                    </Mailto>
                 </h3>
             </div>
         )
